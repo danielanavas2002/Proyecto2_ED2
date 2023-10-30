32ab28955554220f4e89c6b88f4561656d6bd14e
danielanavas2002 <132109593+danielanavas2002@users.noreply.github.com>
2023-10-29 21:00:19 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -12,8 +12,10 @@
 //****************************************************************
 // Variables Globales
 //****************************************************************
-// Distancia en Sensor Ultrasonico
+// Distancia medida por Sensor Ultrasonico
 float distancia;
+float distanciaI;
+float alturaRec = 12.60;
 // Comado cuando se deba enviar un Dato a la Tiva
 int comando;
 //****************************************************************
@@ -36,7 +38,11 @@ void loop() {
     comando = Serial1.read(); // Leer este valor y almacenar en variable comando
   }
   if(comando == '1'){ // Si comando es igual a 1
-    distancia = lectura(trigger, echo); // Se mide la distancia utilizando la Libreria del Sensor
+    distanciaI = lectura(trigger, echo); // Se mide la distancia utilizando la Libreria del Sensor
+    distancia = alturaRec - distanciaI; //Se calcula la altura del nivel del agua sabiendo el alto del recipiente
+    if (distancia < 0){ //Si en algun momento se da una medicion erronea y se tiene un numero negativo
+      distancia = 0.01; //Enviar un dato mayor a 0 para evitar problemas en la Tiva
+    }
     Serial1.println(distancia); // Enviar este dato en el Serial 1 por UART hacia la Tiva
     Serial.print("Dato enviado: "); //Enviar dato en el Serial 0 por UART hacia la computadora
     Serial.print(distancia);
